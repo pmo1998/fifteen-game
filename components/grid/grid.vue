ad42be33a196e6c4292a720170416fdd9457ea89
Leyla <lisana5632@gmail.com>
2019-10-11 11:24:02 +0300

    added package-lock.json and removed tile component

--- components/grid/grid.vue
+++ components/grid/grid.vue
@@ -1,26 +1,21 @@
 <template>
     <div class="grid">
-      <tile v-for="(number, index) in numbers"
-        :key="index"
-        :content="number"
-        :isEmpty="index===emptyIndex"
-        :index="index"
-        @onTileClick="onTileClick"></tile>
+      <div class="tile" v-for="(number, index) in numbers"
+          :key="index"
+          :style="{backgroundColor: index===emptyIndex ? 'white' : '#678EF0'}"
+          @click="onTileClick(index)">
+          <span class="tile__content">{{ index===emptyIndex ? '' : number }}</span>
+        </div>
     </div>
 </template>
 
 <script>
-import Tile from '../tile/tile'
 import * as utils from '~/utils/index'
 
-const GRID_SIZE = 16;
 const EMPTY_VALUE = 0;
 
 export default {
   name: 'grid',
-  components: {
-    'tile': Tile
-  },
   data() {
     return this.getData();
   },
@@ -65,4 +60,23 @@ export default {
   margin-top: 20px;
   box-sizing: border-box;
 }
+
+.tile {
+    width: 90px;
+    height: 90px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 10px 0;
+    box-sizing: border-box;
+    border-radius: 10%;
+    cursor: pointer;
+    transition: background-color .1s ease-in-out;
+}
+.tile__content {
+    display: block;
+    font-size: 30px;
+    font-weight: bold;
+}
+
 </style>
